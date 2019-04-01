b51ce7784d122b8bc3b564d6aaf6f0fce8ceb166
FerGutierrez <fer.gutierrez.santos@gmail.com>
2019-04-01 21:53:19 +0200

    config service

---
+++ src/app/services/config.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, MissingTranslationStrategy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ConfigService {
+
+  constructor(private _http: HttpClient) { }
+
+  fetch(): Observable<any> {
+    return this._http.get('/assets/configs/main.json');
+  }
+
+}
