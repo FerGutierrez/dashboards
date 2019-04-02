dc2e7751b309704f8620149d9007b70c32741326
FerGutierrez <fer.gutierrez.santos@gmail.com>
2019-04-02 02:16:30 +0200

    list dashboards in main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Store } from '@ngrx/store';
 import { IAppState } from './store/state/app.state';
 import { selectConfig, selectConfigLoading, selectConfigError } from './store/selectors/config.selector';
 import { GetConfig } from './store/actions/config.actions';
+import { GetDashboards } from './store/actions/dashboard.actions';
+import { Observable } from 'rxjs';
+import { selectDashboardList } from './store/selectors/dashboard.selectors';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +17,16 @@ export class AppComponent implements OnInit {
   config$ = this.store.select(selectConfig);
   loading$ = this.store.select(selectConfigLoading);
   error$ = this.store.select(selectConfigError);
+  dashboards$ = this.store.select(selectDashboardList);
 
   constructor(private store: Store<IAppState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(new GetConfig());
+
+    this.config$.do(config => console.log(config)).switchMap((config) => {
+      const payload = config.dashboards || [];
+      return Observable.of(new GetDashboards(payload));
+    });
   }
 }
